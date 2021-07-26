66da2578c67233b50aee99d3703d85b6ffe6729f
ve3 <kawewong@gmail.com>
2021-07-26 20:05:04 +0700

    Update tests.

--- tests/phpunit/PHP71/AbstractImageTest.php
+++ tests/phpunit/PHP71/AbstractImageTest.php
@@ -25,35 +25,6 @@ class AbstractImageTest extends \Rundiz\Image\Tests\PHP71\CommonTestAbstractClas
     }// testBuildSourceImageData
 
 
-    public function testGetImageFileData()
-    {
-        $ImgAc = new \Rundiz\Image\Tests\ExtendedAbstractImage(static::$source_images_dir . 'city-amsterdam.jpg');
-        $this->assertTrue(is_array($ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.jpg')));
-        $this->assertTrue(is_array($ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.gif')));
-        $this->assertTrue(is_array($ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.png')));
-        $this->assertTrue(is_array($ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.webp')));
-        $this->assertFalse($ImgAc->getImageFileData(static::$source_images_dir . 'file-not-exists' . date('YmdHis') . '.jpg'));
-        $this->assertFalse($ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam-text.jpg'));
-
-        // go in details.
-        $result = $ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.jpg');
-        $this->assertSame(IMAGETYPE_JPEG, $result[2]);
-        $this->assertSame(2, $result[2]);
-        $result = $ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.gif');
-        $this->assertSame(IMAGETYPE_GIF, $result[2]);
-        $this->assertSame(1, $result[2]);
-        $result = $ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.png');
-        $this->assertSame(IMAGETYPE_PNG, $result[2]);
-        $this->assertSame(3, $result[2]);
-        $result = $ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.webp');
-        $this->assertSame(IMAGETYPE_WEBP, $result[2]);
-        $this->assertSame(18, $result[2]);
-        $this->assertSame('image/webp', $result['mime']);
-        $this->assertSame('.webp', $result['ext']);
-        unset($ImgAc, $result);
-    }// testGetImageFileData
-
-
     public function testGetImageSize()
     {
         $ImgAc = new \Rundiz\Image\Tests\ExtendedAbstractImage(static::$source_images_dir . 'city-amsterdam.jpg');
---
+++ tests/phpunit/PHP71/TraitsTest/ImageTraitTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
+
+namespace Rundiz\Image\Tests\PHP71\TraitsTest;
+
+
+/**
+ * @group processing
+ */
+class ImageTraitTest extends \Rundiz\Image\Tests\PHP71\CommonTestAbstractClass
+{
+
+
+    public function testGetImageFileData()
+    {
+        $ImgAc = new \Rundiz\Image\Tests\ExtendedAbstractImage(static::$source_images_dir . 'city-amsterdam.jpg');
+        $this->assertTrue(is_array($ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.jpg')));
+        $this->assertTrue(is_array($ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.gif')));
+        $this->assertTrue(is_array($ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.png')));
+        $this->assertTrue(is_array($ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.webp')));
+        $this->assertFalse($ImgAc->getImageFileData(static::$source_images_dir . 'file-not-exists' . date('YmdHis') . '.jpg'));
+        $this->assertFalse($ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam-text.jpg'));
+
+        // go in details.
+        $result = $ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.jpg');
+        $this->assertSame(IMAGETYPE_JPEG, $result[2]);
+        $this->assertSame(2, $result[2]);
+        $result = $ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.gif');
+        $this->assertSame(IMAGETYPE_GIF, $result[2]);
+        $this->assertSame(1, $result[2]);
+        $result = $ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.png');
+        $this->assertSame(IMAGETYPE_PNG, $result[2]);
+        $this->assertSame(3, $result[2]);
+        $result = $ImgAc->getImageFileData(static::$source_images_dir . 'city-amsterdam.webp');
+        $this->assertSame(IMAGETYPE_WEBP, $result[2]);
+        $this->assertSame(18, $result[2]);
+        $this->assertSame('image/webp', $result['mime']);
+        $this->assertSame('.webp', $result['ext']);
+        unset($ImgAc, $result);
+    }// testGetImageFileData
+
+
+}
